450b4da7f9351bd05d0cf3e0dd7237d5b474b686
Sadegh Majidi <m.majidi@sharif.edu>
2023-11-21 19:26:48 +0330

    initialize project

---
+++ WeatherApplication.c
@@ -0,0 +1,25 @@
+#include <Uefi.h>
+#include <Library/UefiApplicationEntryPoint.h>
+#include <Library/UefiLib.h>
+
+/**
+  as the real entry point for the application.
+
+  @param[in] ImageHandle    The firmware allocated handle for the EFI image.  
+  @param[in] SystemTable    A pointer to the EFI System Table.
+  
+  @retval EFI_SUCCESS       The entry point is executed successfully.
+  @retval other             Some error occurs when executing this entry point.
+
+**/
+EFI_STATUS
+EFIAPI
+UefiMain (
+  IN EFI_HANDLE        ImageHandle,
+  IN EFI_SYSTEM_TABLE  *SystemTable
+  )
+{
+  Print(L"Hello World \n"); 
+  return EFI_SUCCESS;
+}
+
